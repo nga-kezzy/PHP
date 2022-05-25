9df6e352a597df59e621bed95702f90a2f281d74
ngakezzy <ngakezzy@gmail.com>
2022-05-25 10:00:12 +0700

    thêm trang logout để xóa session rồi trở về trang login

--- admin.php
+++ admin.php
@@ -9,8 +9,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
+    <style>
+        .container{
+            margin:auto;
+            width: 600px;
+            
+        }
+    </style>
 </head>
 <body>
+    
+    <div class="container">
     <?php
     include'control.php';
     $data = new Data();
@@ -19,11 +28,29 @@
     foreach ($user as $value) {
         $get_user = $value['username'];
     }
-    echo"Hello ".$get_user;
+    echo"<h4>Hello</h4> ".$get_user."<br>";
 
-    
+    foreach($user as $value){
+        $get_email = $value['email'];
+    }
+    echo"Email: ".$get_email."<br>"; 
+
+    foreach($user as $value){
+        $get_phone = $value['phone'];
+    }
+    echo"Phone: ".$get_phone."<br>";
+
+
+    echo"Avatar : ";
     
     ?>
+    
+        
+    
+    
+    <img src="upload/<?php echo $_FILES['txt_file']['name']?>" width = "70%"; height = "100%">
+    <a href="logout.php">Logout</a>
+    </div>
 
 
 </body>
---
+++ logout.php
@@ -0,0 +1,18 @@
+<?php
+    session_start();
+    session_destroy();	
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>logout</title>
+</head>
+<body>
+    <?php
+    header("Location:login.php");
+    ?>
+</body>
+</html>
